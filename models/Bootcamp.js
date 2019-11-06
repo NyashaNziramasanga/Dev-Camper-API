aef958c89005b2af06c6caa9fcb09d1a7912abaf
Nyasha Nziramasanga <nash@billowsoftware.com.au>
2019-11-06 22:40:02 +1100

    CRUD for bootcamps

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -86,11 +86,6 @@ const BootcampSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
-  user: {
-    type: mongoose.Schema.ObjectId,
-    ref: 'User',
-    required: true,
-  },
 });
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
