cf51e528edfb54dec0b3b8cf6300aff958c82b51
Nyasha Nziramasanga <nash@billowsoftware.com.au>
2019-11-16 22:19:49 +1100

    AddCourse API and GetSingle Course API

--- controllers/courses.js
+++ controllers/courses.js
@@ -2,6 +2,7 @@
 // API CONTROLLERS
 //
 const Course = require('../models/Course');
+const Bootcamp = require('../models/Bootcamp');
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 
@@ -17,10 +18,44 @@ exports.getCourses = asyncHandler(async (req, res, next) => {
   if (req.params.bootcampId) {
     query = Course.find({ bootcamp: req.params.bootcampId });
   } else {
-    query = Course.find().populate({ path: 'bootcamp', select: 'name description' });
+    query = Course.find().populate('bootcamp', 'name description');
   }
 
   const courses = await query;
 
   res.status(200).json({ success: true, count: courses.length, data: courses });
+});
+
+/** 
+@desc    Get a single course
+@route   GET /api/v1/courses/:id
+@access  Public 
+*/
+exports.getCourse = asyncHandler(async (req, res, next) => {
+  const course = await Course.findById(req.params.id).populate('bootcamp', 'name description');
+
+  if (!course) {
+    return next(new ErrorResponse(`No course with the ifd of ${req.params.id}`), 404);
+  }
+
+  res.status(200).json({ success: true, data: course });
+});
+
+/** 
+@desc    Add course
+@route   POST /api/v1/bootcamps/:bootcampId/courses
+@access  Private 
+*/
+exports.addCourse = asyncHandler(async (req, res, next) => {
+  req.body.bootcamp = req.params.bootcampId;
+
+  const bootcamp = await Bootcamp.findById(req.params.bootcampId);
+
+  if (!bootcamp) {
+    return next(new ErrorResponse(`No course with the ifd of ${req.params.bootcampId}`), 404);
+  }
+
+  const course = await Course.create(req.body);
+
+  res.status(200).json({ success: true, data: course });
 });
