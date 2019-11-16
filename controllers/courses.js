38f85597661bc187af06322dd0ae0530b9a35b46
Nyasha Nziramasanga <nash@billowsoftware.com.au>
2019-11-16 22:36:46 +1100

    Update course API

--- controllers/courses.js
+++ controllers/courses.js
@@ -52,10 +52,30 @@ exports.addCourse = asyncHandler(async (req, res, next) => {
   const bootcamp = await Bootcamp.findById(req.params.bootcampId);
 
   if (!bootcamp) {
-    return next(new ErrorResponse(`No course with the ifd of ${req.params.bootcampId}`), 404);
+    return next(new ErrorResponse(`No course with the id of ${req.params.bootcampId}`), 404);
   }
 
   const course = await Course.create(req.body);
+
+  res.status(200).json({ success: true, data: course });
+});
+
+/** 
+@desc    Update course
+@route   PUT /api/v1/courses/:id
+@access  Private 
+*/
+exports.updateCourse = asyncHandler(async (req, res, next) => {
+  let course = await Course.findById(req.params.id);
+
+  if (!course) {
+    return next(new ErrorResponse(`No course with the id of ${req.params.id}`), 404);
+  }
+
+  course = await Course.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
 
   res.status(200).json({ success: true, data: course });
 });
