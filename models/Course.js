0b634454711293d8a8cae32105d5540e3a820f85
Nyasha Nziramasanga <nash@billowsoftware.com.au>
2019-11-18 01:27:49 +1100

    Calculating average course cost with Aggregate

--- models/Course.js
+++ models/Course.js
@@ -38,4 +38,40 @@ const CourseSchema = new mongoose.Schema({
   },
 });
 
+// Static method eto get average of course tuitions
+CourseSchema.statics.getAverageCost = async function(bootcampId) {
+  console.log(`Calculating average cost`.blue.inverse);
+
+  const obj = await this.aggregate([
+    {
+      $match: { bootcamp: bootcampId },
+    },
+    {
+      $group: {
+        _id: '$bootcamp',
+        averageCost: { $avg: '$tuition' },
+      },
+    },
+  ]);
+
+  try {
+    // Grab Bootcamp model and update bootcamp by ID and add average course
+    await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
+      averageCost: Math.ceil(obj[0].averageCost / 10) * 10,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// Call getAverage cost after save
+CourseSchema.post('save', function() {
+  this.constructor.getAverageCost(this.bootcamp);
+});
+
+// Call getAverage cost before remove
+CourseSchema.pre('remove', function() {
+  this.constructor.getAverageCost(this.bootcamp);
+});
+
 module.exports = mongoose.model('Course', CourseSchema);
