ae2ce5c1edb8e2a52412c71c7667726635077944
Nyasha Nziramasanga <nash@billowsoftware.com.au>
2019-11-19 01:22:58 +1100

    mddleware to handle paginationand advanced filtering

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -13,68 +13,7 @@ const asyncHandler = require('../middleware/async');
 @access  Public 
 */
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-  //Get query string in URL and add $ for use by mongoose
-  let query;
-
-  // Copy req.query
-  const reqQuery = { ...req.query };
-
-  // Fields to exclude
-  const removeFields = [ 'select', 'sort', 'page', 'limit' ];
-
-  // Loop over removeFields and delete them form reqQuery
-  removeFields.forEach((param) => delete reqQuery[param]);
-  console.log(reqQuery);
-
-  // Create Query string
-  let queryStr = JSON.stringify(reqQuery);
-
-  // Create operators ($gt, $gte, etc)
-  queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, (match) => `$${match}`);
-
-  // Finding resources
-  query = Bootcamp.find(JSON.parse(queryStr)).populate('courses');
-
-  // Select fields
-  if (req.query.select) {
-    const fields = req.query.select.split(',').join(' ');
-    query = query.select(fields);
-  }
-  if (req.query.sort) {
-    const sortBy = req.query.sort.split(',').join(' ');
-    query = query.sort(sortBy);
-  } else {
-    query = query.sort('-createdAt');
-  }
-
-  // Pagination
-  const page = parseInt(req.query.page, 10) || 1;
-  const limit = parseInt(req.query.limit, 10) || 10;
-  const startIndex = (page - 1) * limit;
-  const endIndex = page * limit;
-  const total = await Bootcamp.find(JSON.parse(queryStr)).countDocuments();
-
-  query = query.skip(startIndex).limit(limit);
-
-  // Pagination result
-  const pagination = {};
-  if (endIndex < total) {
-    pagination.next = {
-      page: page + 1,
-      limit,
-    };
-  }
-
-  if (startIndex > 0) {
-    pagination.prev = {
-      page: page - 1,
-      limit,
-    };
-  }
-
-  // Executing query
-  const bootcamps = await query;
-  res.status(200).json({ success: true, count: bootcamps.length, pagination, data: bootcamps });
+  res.status(200).json(res.advancedResults);
 });
 
 /**
--- middleware/advancedResult.js
+++ middleware/advancedResult.js
@@ -1,3 +1,78 @@
-const advancedResults = (model, populate) => async (req, res, next) => {};
+const advancedResults = (model, populate) => async (req, res, next) => {
+  //Get query string in URL and add $ for use by mongoose
+  let query;
 
-module.exports;
+  // Copy req.query
+  const reqQuery = { ...req.query };
+
+  // Fields to exclude
+  const removeFields = [ 'select', 'sort', 'page', 'limit' ];
+
+  // Loop over removeFields and delete them form reqQuery
+  removeFields.forEach((param) => delete reqQuery[param]);
+  console.log(reqQuery);
+
+  // Create Query string
+  let queryStr = JSON.stringify(reqQuery);
+
+  // Create operators ($gt, $gte, etc)
+  queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, (match) => `$${match}`);
+
+  // Finding resources
+  query = model.find(JSON.parse(queryStr));
+
+  // Select fields
+  if (req.query.select) {
+    const fields = req.query.select.split(',').join(' ');
+    query = query.select(fields);
+  }
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ');
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort('-createdAt');
+  }
+
+  // Pagination
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 10;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const total = await model.find(JSON.parse(queryStr)).countDocuments();
+
+  query = query.skip(startIndex).limit(limit);
+
+  if (populate) {
+    query = query.populate(populate);
+  }
+
+  // Pagination result
+  const pagination = {};
+  if (endIndex < total) {
+    pagination.next = {
+      page: page + 1,
+      limit,
+    };
+  }
+
+  if (startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit,
+    };
+  }
+
+  // Executing query
+  const results = await query;
+
+  res.advancedResults = {
+    success: true,
+    count: results.length,
+    pagination,
+    data: results,
+  };
+
+  next();
+};
+
+module.exports = advancedResults;
