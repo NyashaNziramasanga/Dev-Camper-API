cea30a17add3a3ab45781755cda82b9db9527a40
Nyasha Nziramasanga <nash@billowsoftware.com.au>
2019-12-08 19:50:47 +1100

    Bootcamp Ownership
    - Who can delete and update bootcamps

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -64,14 +64,23 @@ exports.createBootcamp = asyncHandler(async (req, res, next) => {
 @access  Private 
 */
 exports.updateBootcamp = asyncHandler(async (req, res, next) => {
-  const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-    runValidators: true,
-  });
+  let bootcamp = await Bootcamp.findById(req.params.id);
 
   if (!bootcamp) {
     return next(new ErrorResponse(`Bootcamps not found with id of ${req.params.id}`, 404));
   }
+
+  // Make sure user is bootcamp owner
+  if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(`User ${req.params.id} is not authorized to update this bootcamp`, 404)
+    );
+  }
+
+  bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
 
   res.status(200).json({ success: true, data: bootcamp });
 });
@@ -86,6 +95,13 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
 
   if (!bootcamp) {
     return next(new ErrorResponse(`Bootcamps not found with id of ${req.params.id}`, 404));
+  }
+
+  // Make sure user is bootcamp owner
+  if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(`User ${req.params.id} is not authorized to delete this bootcamp`, 404)
+    );
   }
 
   bootcamp.remove();
@@ -132,6 +148,13 @@ exports.bootcampPhotoUpload = asyncHandler(async (req, res, next) => {
 
   if (!bootcamp) {
     return next(new ErrorResponse(`Bootcamps not found with id of ${req.params.id}`, 404));
+  }
+
+  // Make sure user is bootcamp owner
+  if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(`User ${req.params.id} is not authorized to update this bootcamp`, 404)
+    );
   }
 
   if (!req.files) {
