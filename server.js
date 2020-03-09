8ba8afa4ecd1c284bd376645c14fc3465de8db94
Nyasha Nziramasanga <nash@billowsoftware.com.au>
2020-03-09 23:15:03 +1030

    xpress-rate-limit and hpp security installs
    Installed hpp to prevent http pollution
    Installed xpress-rate-limit to 100 limit

--- server.js
+++ server.js
@@ -7,9 +7,13 @@ const colors = require('colors');
 const fileupload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
 const errorHandler = require('./middleware/error');
+
+// Security middleware
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 const xss = require('xss-clean');
+const hpp = require('hpp');
+const rateLimit = require('express-rate-limit');
 
 //Load env vars
 dotenv.config({ path: './config/config.env.env' });
@@ -48,6 +52,17 @@ app.use(helmet());
 
 // Prevent XSS attacks
 app.use(xss());
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, //10min
+  max: 100
+});
+
+app.use(limiter);
+
+// Prevent HTTP param pollution
+app.use(hpp());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
