7b42bd0e79d24e0ea3ba43e1ff9db730953e8806
Nyasha Nziramasanga <nash@billowsoftware.com.au>
2020-03-09 22:27:12 +1030

    Security implemetation of express-mongo-sanitize
    - Installed express-mongo-sanitize

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const colors = require('colors');
 const fileupload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
 const errorHandler = require('./middleware/error');
+const mongoSanitize = require('express-mongo-sanitize');
 
 //Load env vars
 dotenv.config({ path: './config/config.env.env' });
@@ -31,11 +32,14 @@ app.use(cookieParser());
 
 //Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
-	app.use(morgan('dev'));
+  app.use(morgan('dev'));
 }
 
 // File  Uploading
 app.use(fileupload());
+
+// Sanitize data
+app.use(mongoSanitize());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
@@ -51,17 +55,17 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 const server = app.listen(
-	PORT,
-	console.log(
-		`🔥 Server running in ${process.env.NODE_ENV} mode on port ${PORT}🔥`.yellow
-			.bold
-	)
+  PORT,
+  console.log(
+    `🔥 Server running in ${process.env.NODE_ENV} mode on port ${PORT}🔥`.yellow
+      .bold
+  )
 );
 
 //Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
-	console.log(`Error : ${err.message}`.red);
+  console.log(`Error : ${err.message}`.red);
 
-	//close server and exit process
-	server.close(() => process.exit(1));
+  //close server and exit process
+  server.close(() => process.exit(1));
 });
