cd2133524982d7790d4d5413a462fb0dfdbf7720
Nyasha Nziramasanga <nash@billowsoftware.com.au>
2020-03-09 22:58:58 +1030

    Security installs helmet and XSS-clean
    - Installed helmet
    - Installed XSS-clean

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const fileupload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
 const errorHandler = require('./middleware/error');
 const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
 
 //Load env vars
 dotenv.config({ path: './config/config.env.env' });
@@ -40,6 +42,12 @@ app.use(fileupload());
 
 // Sanitize data
 app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
